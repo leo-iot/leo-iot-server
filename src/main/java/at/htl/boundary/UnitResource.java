90b7c6a6837883c49d4b984294f1ce5cea9b9529
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-10-12 20:49:04 +0200

    refactor unit ressource #leoiot-130

--- src/main/java/at/htl/boundary/UnitResource.java
+++ src/main/java/at/htl/boundary/UnitResource.java
@@ -1,11 +1,13 @@
 package at.htl.boundary;
 
+import at.htl.entity.Location;
 import at.htl.entity.Unit;
 import at.htl.repository.UnitRepository;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -24,8 +26,12 @@ public class UnitResource {
     )
     public Response getUnit(@QueryParam("id") Long unitId ){
         if(unitId != null){
+            Unit u = unitRepository.findById(unitId);
+            if(u == null){
+                return Response.status(404).build();
+            }
             return Response
-                    .accepted(unitRepository.findById(unitId))
+                    .accepted(u)
                     .build();
         }else {
             return Response
@@ -40,6 +46,7 @@ public class UnitResource {
             summary = "save a unit",
             description = "save the desired unit object"
     )
+    @Transactional
     public Response addUnit(Unit unit){
         return Response
                 .accepted(unitRepository.save(unit))
@@ -51,9 +58,26 @@ public class UnitResource {
             summary = "delete a unit",
             description = "delete the desired unit by id"
     )
+    @Transactional
     public Response deleteUnitById(@QueryParam("id") Long unitId){
         return Response
                 .accepted(unitRepository.deleteById(unitId))
+                .build();
+    }
+
+    @PUT
+    @Operation(
+            summary = "update a unit",
+            description = "update the desired unit"
+    )
+    @Transactional
+    public Response updateUnit(Unit unit){
+        Unit u = unitRepository.updateUnit(unit);
+        if(u == null){
+            return Response.status(404).build();
+        }
+        return Response
+                .accepted(u)
                 .build();
     }
 
