8a0ab37555637998f3b195873b31ddf923142db6
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-10-12 21:17:49 +0200

    refactor SensorTypeResource #leoiot-130

--- src/main/java/at/htl/boundary/SensorTypeResource.java
+++ src/main/java/at/htl/boundary/SensorTypeResource.java
@@ -2,12 +2,14 @@ package at.htl.boundary;
 
 import at.htl.entity.Location;
 import at.htl.entity.SensorType;
+import at.htl.entity.Unit;
 import at.htl.repository.SensorTypeRepository;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -21,17 +23,21 @@ public class SensorTypeResource {
 
     @GET
     @Operation(
-            summary = "get a SensorTYpe",
+            summary = "get a SensorType",
             description ="get the desired sensortype by id"
     )
     public Response getSensorType(@QueryParam("id") Long sensorTypeId){
         if (sensorTypeId != null){
+            SensorType st = sensorTypeRepository.findById(sensorTypeId);
+            if(st == null){
+                return Response.status(404).build();
+            }
             return Response
-                    .accepted(sensorTypeRepository.findById(sensorTypeId))
+                    .accepted(st)
                     .build();
         }else {
             return Response
-                    .accepted(sensorTypeRepository.findAll())
+                    .accepted(sensorTypeRepository.listAll())
                     .build();
         }
     }
@@ -42,6 +48,7 @@ public class SensorTypeResource {
             summary = "save a sensortype",
             description = "save the desired sensortype"
     )
+    @Transactional
     public Response addSensorType(SensorType sensorType){
         return Response
                 .accepted(sensorTypeRepository.save(sensorType))
@@ -54,10 +61,26 @@ public class SensorTypeResource {
             summary = "delete a actor",
             description = "delete the desired sensorType by id"
     )
+    @Transactional
     public Response deleteSensorTypeById(@QueryParam("id") Long sensorTypeId){
         return Response
                 .accepted(sensorTypeRepository.deleteById(sensorTypeId))
                 .build();
     }
 
+    @PUT
+    @Operation(
+            summary = "update a unit",
+            description = "update the desired unit"
+    )
+    @Transactional
+    public Response updateSensorType(SensorType sensorType){
+        SensorType st = sensorTypeRepository.updateSensorType(sensorType);
+        if(st == null){
+            return Response.status(404).build();
+        }
+        return Response
+                .accepted(st)
+                .build();
+    }
 }
